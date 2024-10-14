969c081bd00894a67826b031f7552425ebb1330d
Oliwia Majewska <oliwiam602@gmail.com>
2024-10-14 08:29:12 +0000

    Dodano program kalkulator w pythonie

---
+++ kalkulator.py
@@ -0,0 +1,21 @@
+# Uruchamiamy Python interaktywnie
+print(23 + 34)  # To wyświetli 57
+
+# Operacja na wyniku (23 + 34) - 12
+print(57 - 12)  # To wyświetli 45
+
+# Przypisanie wartości do zmiennych
+a = 2
+b = 5
+print(a * b)  # To wyświetli 10 (2 * 5)
+
+# Użycie łamania wiersza za pomocą backslash
+print(1 + 2 + \
+      4 - 3)  # To wyświetli 4 (1 + 2 + 4 - 3)
+
+# Użycie nawiasów do grupowania długich wyrażeń
+print(10 + 12 + 
+      13)  # To wyświetli 35 (10 + 12 + 13)
+
+# Zakończenie pracy z Pythonem interaktywnie
+# quit() - funkcja do zakończenia sesji interaktywnej (w interaktywnym trybie Pythona)
